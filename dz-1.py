e91ffface91e54804e99a04f9a32920baf9c1688
gerhat87 <gerhat@mail.ru>
2024-08-28 11:35:00 +0300

    Delete dz-1.py

--- dz-1.py
+++
@@ -1,9 +0,0 @@
-import requests
-import pprint
-params = {
-    'q': "html"
-}
-response = requests.get("https://api.github.com", params = params)
-response_json = response.json()
-print(response.status_code)
-pprint.pprint(response_json)
